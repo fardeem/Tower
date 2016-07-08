e1982a24ad932414e4553450878d9d9b6e57a818
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-08 07:52:06 +0600

    Wire subject list with the redux store to enable searching.
    `connect` from `react-redux` fetches the search string from the store,
    which is passed into a container component that builds a query and
    passes it into the db call.

--- imports/ui/containers/SubjectsListContainer.js
+++ imports/ui/containers/SubjectsListContainer.js
@@ -1,11 +1,19 @@
+import { connect } from 'react-redux';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Subjects } from '../../api/models/subjects.js';
 import SubjectsList from '../components/SubjectsList.js';
 
-const SubjectsListContainer = createContainer(() => ({
-  subjects: Subjects.find().fetch(),
-}), SubjectsList);
 
-export default SubjectsListContainer;
+const SubjectsListContainer = createContainer(({ pageSearch }) => {
+  let query = {};
+  query = pageSearch.trim() ? { name: new RegExp(`^${pageSearch}`, 'gi') } : {};
 
+  return {
+    subjects: Subjects.find(query).fetch(),
+  };
+}, SubjectsList);
+
+export default connect(
+  ({ pageSearch }) => ({ pageSearch })
+)(SubjectsListContainer);
