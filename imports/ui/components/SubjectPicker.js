204a6a0d89a2b22ca62a7f944e453e4c52e40ac3
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-09 17:49:47 +0600

    Reusable subject picker component.

---
+++ imports/ui/components/SubjectPicker.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Select from 'react-select';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Subjects } from '../../api/models/subjects.js';
+
+
+const SubjectPicker = ({ value, options, onChange }) => {
+  function formatChange(data) {
+    return onChange(data.map(item => item.value));
+  }
+
+  return (
+    <Select
+      name="subject-picker"
+      value={value}
+      options={options}
+      multi
+      onChange={formatChange}
+    />
+  );
+};
+
+SubjectPicker.propTypes = {
+  value: React.PropTypes.array,
+  options: React.PropTypes.array,
+  onChange: React.PropTypes.func,
+};
+
+
+export default createContainer(({ selected = [], onChange }) => {
+  const mapToProps = ({ _id, name, grade }) => ({
+    value: _id,
+    label: `Gr. ${grade} — ${name}`,
+  });
+
+  const options = { sort: { grade: 1 } };
+
+  return {
+    options: Subjects.find({}, options).map(mapToProps),
+    value: Subjects.find({ _id: { $in: selected } }, options).map(mapToProps),
+    onChange,
+  };
+}, SubjectPicker);
