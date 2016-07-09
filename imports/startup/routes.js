0aebbddd7b91921684d32e7a6e1d52c0fd5cd3c0
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-09 17:39:36 +0600

    Clear search value in the store when the route changes.

--- imports/startup/routes.js
+++ imports/startup/routes.js
@@ -8,12 +8,16 @@ import SubjectsPage from '../ui/pages/SubjectsPage.js';
 import TeachersPage from '../ui/pages/TeachersPage.js';
 
 
+function clearState() {
+  return store.dispatch({ type: 'CHANGE_SEARCH_TEXT', text: '' });
+}
+
 export const renderRoutes = () => (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={AppContainer}>
-        <Route path="subjects" component={SubjectsPage} />
-        <Route path="teachers" component={TeachersPage} />
+        <Route path="subjects" component={SubjectsPage} onLeave={clearState} />
+        <Route path="teachers" component={TeachersPage} onLeave={clearState} />
       </Route>
     </Router>
   </Provider>
