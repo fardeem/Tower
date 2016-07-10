25a484710eaa00217fc03ac9ac4acf3b18ab5442
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-10 10:04:47 +0600

    Import collection definition.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/models/subjects.js';
+import '../imports/api/models/teachers.js';
+
 
 Meteor.startup(() => {
   // code to run on server at startup
