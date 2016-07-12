1f085da93b977dfd94367e44695e8919b8c086a6
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-12 08:20:45 +0600

    Import exam collection definition to the server.

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/models/subjects.js';
 import '../imports/api/models/teachers.js';
-
+import '../imports/api/models/exams.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
