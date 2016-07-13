fcb83e9f69e0ebe74495f54cedc9d42f6f06ccd2
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-13 19:49:38 +0600

    Reset navigation when the route changes.

--- imports/startup/routes.js
+++ imports/startup/routes.js
@@ -16,6 +16,14 @@ const Rooms = () => (
   <p>ROOMS PAGE</p>
 );
 
+/* Methods */
+function restoreNav() {
+  return Store.dispatch({ type: 'CHANGE_NAV', text: '' });
+}
+
+
+/* Def */
+
 export const renderRoutes = () => (
   <Provider store={Store}>
     <Router history={browserHistory}>
@@ -23,7 +31,7 @@ export const renderRoutes = () => (
         <Route path="subjects" component={SubjectsPage} />
         <Route path="teachers" component={TeachersPage} />
 
-        <Route path="exams/:examId" component={ExamsPage} >
+        <Route path="exams/:examId" component={ExamsPage} onLeave={restoreNav} >
           <IndexRoute component={Routine} />
           <Route path="rooms" component={Rooms} />
         </Route>
