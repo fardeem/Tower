c8aa36f0025dffe2b2bd9b9f0df3693a35289235
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-13 19:49:00 +0600

    Define mapDispatchToProps inline with connect defintion.

--- imports/ui/containers/NavSearchContainer.js
+++ imports/ui/containers/NavSearchContainer.js
@@ -3,15 +3,13 @@ import { connect } from 'react-redux';
 import NavSearch from '../components/NavSearch.js';
 
 
-const mapDispatchToProps = (dispatch) => ({
-  handleChange({ target }) {
-    return dispatch({ type: 'CHANGE_SEARCH_TEXT', text: target.value });
-  },
-});
-
 const NavSearchContainer = connect(
   ({ pageSearch }) => ({ pageSearch }),
-  mapDispatchToProps
+  (dispatch) => ({
+    handleChange({ target }) {
+      return dispatch({ type: 'CHANGE_SEARCH_TEXT', text: target.value });
+    },
+  })
 )(NavSearch);
 
 export default NavSearchContainer;
