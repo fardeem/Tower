9a44dcad4af899e848d5c8268a8ac54805beb0d7
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-14 17:29:55 +0600

    Remove empty subject object, a side effect of the map function.

--- imports/ui/containers/SessionsListContainer.js
+++ imports/ui/containers/SessionsListContainer.js
@@ -9,7 +9,7 @@ const SessionsListContainer = createContainer(({ day, grade, startTime }) => {
   const subjects = Sessions.find({ day })
     .map(({ subjectId, room }) => (
       Object.assign({}, Subjects.findOne({ _id: subjectId, grade }), { room })
-    ));
+    )).filter(({ _id }) => !! _id);
 
   return { subjects, startTime };
 }, SessionsList);
