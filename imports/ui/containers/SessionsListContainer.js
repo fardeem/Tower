24ad7c2f2493348b9d7eaa53d00132ee882b86ae
Fardeem Munir <fardeem.munir@erubai.com>
2016-07-14 17:57:41 +0600

    Merge session id with the subject object before returning.

--- imports/ui/containers/SessionsListContainer.js
+++ imports/ui/containers/SessionsListContainer.js
@@ -7,8 +7,11 @@ import SessionsList from '../components/SessionsList.js';
 
 const SessionsListContainer = createContainer(({ day, grade, startTime }) => {
   const subjects = Sessions.find({ day })
-    .map(({ subjectId, room }) => (
-      Object.assign({}, Subjects.findOne({ _id: subjectId, grade }), { room })
+    .map(({ _id, subjectId, room }) => (
+      Object.assign({},
+        Subjects.findOne({ _id: subjectId, grade }),
+        { sessionId: _id, room }
+      )
     )).filter(({ _id }) => !! _id);
 
   return { subjects, startTime };
